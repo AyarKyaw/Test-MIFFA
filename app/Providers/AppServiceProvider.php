9f368b328be75864ed432bde43f8380e403004d0
Ayar Kyaw <rooney.ayarkyaw@gmail.com>
2026-09-06 19:05:16 +0630

    Updating Email Provide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Transport\AbstractTransport;
+use Symfony\Component\Mailer\SentMessage;
+use Google\Client as GoogleClient;
+use Google\Service\Gmail;
+use Google\Service\Gmail\Message;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +29,34 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production' || request()->server('HTTP_X_FORWARDED_PROTO') === 'https') {
             URL::forceScheme('https');
         }
+
+        Mail::extend('gmail', function (array $config = []) {
+            return new class extends AbstractTransport {
+                protected function doSend(SentMessage $message): void
+                {
+                    $email = $message->getOriginalMessage();
+
+                    $client = new GoogleClient();
+                    $client->setClientId(config('services.gmail.client_id'));
+                    $client->setClientSecret(config('services.gmail.client_secret'));
+                    $client->refreshToken(config('services.gmail.refresh_token'));
+
+                    $service = new Gmail($client);
+
+                    $rawMessage = base64_encode($email->toString());
+                    $rawMessage = str_replace(['+', '/', '='], ['-', '_', ''], $rawMessage);
+
+                    $gmailMessage = new Message();
+                    $gmailMessage->setRaw($rawMessage);
+
+                    $service->users_messages->send('me', $gmailMessage);
+                }
+
+                public function __toString(): string
+                {
+                    return 'gmail_api';
+                }
+            };
+        });
     }
 }
